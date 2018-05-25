db41a1431ad54f77410909404a2993d3894b62d0
DemonPenguin325 <demonpenguin325@gmail.com>
2018-05-24 19:01:15 -0600

    Added Path finding

--- NEO3D/Assets/Scripts/NeoLegs.cs
+++ NEO3D/Assets/Scripts/NeoLegs.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NeoLegs : MonoBehaviour {
 	
@@ -10,28 +11,29 @@ public class NeoLegs : MonoBehaviour {
 	public float gravity = 20.0F;
 
 	private Vector3 moveDirection = Vector3.zero;
-	private CharacterController controller;
 	private Rigidbody rig;
 
+    NavMeshAgent agent;
+
 	void Start(){
-		controller = GetComponent<CharacterController>();
 		rig = GetComponent<Rigidbody> ();
+        agent = GetComponent<NavMeshAgent>();
 	}
 
 	void Update() {
 		transform.Rotate (new Vector3 (0, Input.GetAxis("Horizontal"), 0) * rotationSpeed);
-		/*if (controller.isGrounded) {
-			moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-			//if (Input.GetButton("Jump"))
-			//	moveDirection.y = jumpSpeed;
 
-		}
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);*/
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
 
 
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                agent.destination = hit.point;
+                Debug.DrawRay(Camera.main.transform.position, hit.point, Color.red, 3f);
+            }
+        }
 	}
 
 	void FixedUpdate(){
