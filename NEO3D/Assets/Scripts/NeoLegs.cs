e2bdbad10ec63e4d01dc147cf10ce1014d9238a2
DemonPenguin325 <demonpenguin325@gmail.com>
2018-05-31 19:14:33 -0600

    Added 2 commands
    Dance and Stop

--- NEO3D/Assets/Scripts/NeoLegs.cs
+++ NEO3D/Assets/Scripts/NeoLegs.cs
@@ -9,6 +9,7 @@ public class NeoLegs : MonoBehaviour {
 	public float rotationSpeed = 2f;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
+    bool dancing;
 
 	private Vector3 moveDirection = Vector3.zero;
 	private Rigidbody rig;
@@ -31,5 +32,16 @@ public class NeoLegs : MonoBehaviour {
                 Debug.DrawRay(Camera.main.transform.position, hit.point, Color.red, 3f);
             }
         }
+        if (dancing)
+            transform.Rotate(new Vector3(1,1,1));
 	}
+
+    public void Dance()
+    {
+        dancing = true;
+    }
+    public void Stop()
+    {
+        dancing = false;
+    }
 }
--- NEO3D/Assets/Scripts/Parser.cs
+++ NEO3D/Assets/Scripts/Parser.cs
@@ -7,20 +7,35 @@ using UnityEngine.EventSystems;
 public class Parser : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
     InputField commandLine;
+    NeoLegs neo;
     public static bool focused;
     //public static InputField prompt;
 	// Use this for initialization
 	void Awake () {
         commandLine = GetComponent<InputField>();
+        neo = FindObjectOfType<NeoLegs>();
         //prompt = commandLine;
         //Parser.focused = prompt.isFocused;
 	}
 	
 	// Update is called once per frame
     void Update () {
-		if (Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            
+            string command = commandLine.text;
+            commandLine.text = "";
+
+            string[] words = command.Split();
+            for (int i = 0; i < words.Length; i++)
+            {
+                //NLP Code here
+                if (words[i] == "Dance")
+                {
+                    neo.Dance();
+                }
+                else if (words[i] == "Stop")
+                    neo.Stop();
+            }
         }
 	}
 
